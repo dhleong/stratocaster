f1e921f70210fd46cf4181dc31ad53a182aef326
Daniel Leong <me@dhleong.net>
2022-09-07 21:41:21 -0400

    Fill out IMediaStatus type + implement the whole receive loop

--- src/util/protocol.ts
+++ src/util/protocol.ts
@@ -25,11 +25,62 @@ export interface IReceiverApp {
 
 export interface IReceiverStatus {
     applications: IReceiverApp[];
-    userEq: unknown,
+    userEq: unknown;
     volume: {
-        controlType: "attenuation",
-        level: number,
-        muted: boolean,
-        stepInterval: number,
-    }
+        controlType: "attenuation";
+        level: number;
+        muted: boolean;
+        stepInterval: number;
+    };
 }
+
+export enum PlayerState {
+    BUFFERING = "BUFFERING",
+    IDLE = "IDLE",
+    PAUSED = "PAUSED",
+    PLAYING = "PLAYING",
+}
+
+export interface IMediaInformation {
+    breakClips?: unknown[];
+    breaks?: unknown[];
+    contentId: string;
+    contentType: string;
+    contentUrl?: string;
+    customData: Record<string, unknown>;
+    duration?: number;
+    entity?: string;
+    mediaCategory?: "AUDIO" | "VIDEO" | "IMAGE";
+    metadata?: Record<string, unknown>;
+    startAbsoluteTime?: number;
+    streamType: "BUFFERED" | "LIVE" | "NONE";
+    textTrackStyle?: unknown;
+    tracks?: unknown[];
+    userActionStates?: unknown[];
+    vmapAdsRequest?: unknown;
+}
+
+export interface IMediaStatus {
+    activeTrackIds?: number[];
+    breakStatus?: unknown; // TODO
+    currentItemId?: number;
+    currentTime?: number;
+    customData?: Record<string, unknown>;
+    extendedStatus?: unknown; // TODO
+    idleReason?: unknown; // TODO
+    items?: unknown[]; // TODO
+    liveSeekableRange?: unknown;
+    media?: IMediaInformation;
+    mediaSessionId: number;
+    playbackRate: number;
+    playerState: PlayerState;
+    preloadedItemId?: number;
+    supportedMediaCommands: number;
+    videoInfo?: unknown;
+    volume: {
+        level: number;
+        muted: boolean;
+    };
+}
+
+export type IMediaStatuses = IMediaStatus[];
